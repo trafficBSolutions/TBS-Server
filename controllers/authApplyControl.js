389132f4b2c101993302b4caaac76950ea9de593
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-05-09 08:06:04 -0400

    Update authApplyControl.js

--- controllers/authApplyControl.js
+++ controllers/authApplyControl.js
@@ -2,6 +2,7 @@ const Application = require('../models/applyuser');
 const transporter2 = require('../utils/emailConfig'); // Use transporter2 only
 const myEmail = 'tbsolutions9@gmail.com';
 const userEmail = 'tbsolutions4@gmail.com';
+const mainEmail = 'tbsolutions3@gmail.com';
 
 
 const submitApplication = async (req, res) => {
@@ -72,6 +73,7 @@ const submitApplication = async (req, res) => {
             bcc: [
                 { name: 'Traffic & Barrier Solutions, LLC', address: myEmail },
                 { name: 'Carson Speer', address: userEmail }, // Add the second Gmail address to BCC
+                { name: 'Bryson Davis', address: mainEmail }
             ],
             subject: 'JOB APPLICATION REQUEST',
             html: `
