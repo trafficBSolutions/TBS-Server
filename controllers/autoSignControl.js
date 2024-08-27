0ee6aba9d6f96968962ed2627bf71cfbe82aa615
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-08-27 11:20:09 -0400

    Update autoSignControl.js

--- controllers/autoSignControl.js
+++ controllers/autoSignControl.js
@@ -1,8 +1,9 @@
 const SignUser = require('../models/signuser');
-const transporter3 = require('../utils/emailConfig'); // Use transporter2 only
+const transporter4 = require('../utils/emailConfig'); // Use transporter2 only
 const myEmail = 'tbsolutions9@gmail.com';
 const userEmail = 'tbsolutions4@gmail.com';
 const mainEmail = 'tbsolutions3@gmail.com';
+const foreemail = 'tbsolutions55@gmail.com';
 
 const submitSign = async (req, res) => {
     try {
@@ -70,7 +71,8 @@ const submitSign = async (req, res) => {
             bcc: [
                 { name: 'Traffic & Barrier Solutions, LLC', address: myEmail },
                 { name: 'Carson Speer', address: userEmail }, // Add the second Gmail address to BCC
-                { name: 'Bryson Davis', address: mainEmail }
+                { name: 'Bryson Davis', address: mainEmail },
+                { name: 'Jonkell Tolbert', address: foreemail }
             ],
             subject: 'TRAFFIC SIGN REQUEST REQUEST',
             html: `
@@ -351,7 +353,7 @@ const submitSign = async (req, res) => {
         };
 
         // Send email
-        transporter3.sendMail(mailOptions, (error, info) => {
+        transporter4.sendMail(mailOptions, (error, info) => {
             if (error) {
                 console.log('Error sending email notification:', error);
             } else {
