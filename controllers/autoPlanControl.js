75c6d0ec50d05b1e1482f75a1430757b39bf9627
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-08-27 11:17:34 -0400

    Update autoPlanControl.js

--- controllers/autoPlanControl.js
+++ controllers/autoPlanControl.js
@@ -1,8 +1,9 @@
 const PlanUser = require('../models/planuser');
-const transporter3 = require('../utils/emailConfig'); // Use transporter2 only
+const transporter4 = require('../utils/emailConfig'); // Use transporter2 only
 const myEmail = 'tbsolutions9@gmail.com';
 const userEmail = 'tbsolutions4@gmail.com';
 const mainEmail = 'tbsolutions3@gmail.com';
+const foreemail = 'tbsolutions55@gmail.com';
 
 const submitPlan = async (req, res) => {
     try {
@@ -64,7 +65,8 @@ const submitPlan = async (req, res) => {
             bcc: [
                 { name: 'Traffic & Barrier Solutions, LLC', address: myEmail },
                 { name: 'Carson Speer', address: userEmail }, // Add the second Gmail address to BCC
-                { name: 'Bryson Davis', address: mainEmail }
+                { name: 'Bryson Davis', address: mainEmail },
+                { name: 'Jonkell Tolbert', address: foreemail }
             ],
             subject: 'TRAFFIC CONTROL PLAN REQUEST',
             html: `
@@ -279,7 +281,7 @@ const submitPlan = async (req, res) => {
         }
         
 
-        transporter3.sendMail(mailOptions, (error, info) => {
+        transporter4.sendMail(mailOptions, (error, info) => {
             if (error) {
                 console.log('Error sending email notification:', error);
             } else {
