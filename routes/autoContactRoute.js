88c1441f9f7c3a1fa1e5354cef15f7cf14778205
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-12-16 11:30:26 -0500

    Add files via upload

---
+++ routes/autoContactRoute.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { submitContact } = require('../controllers/autoContactControl');
+
+router.use(
+    cors({
+        credentials: true,
+        origin: 'http://localhost:5173'
+    })
+);
+
+// Use bodyParser to parse URL-encoded and JSON data
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+router.post('/contact-us', submitContact);
+
+module.exports = router;
