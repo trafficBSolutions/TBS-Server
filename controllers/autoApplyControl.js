ec1d9dc63bc1fb1b40e552bda761e6f093c26240
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2025-03-20 08:56:56 -0400

    Update autoApplyControl.js

--- controllers/autoApplyControl.js
+++ controllers/autoApplyControl.js
@@ -1,11 +1,11 @@
 const Apply = require("../models/newapply");
 const transporter4 = require("../utils/emailConfig");
 const myEmail = "tbsolutions9@gmail.com";
-/*
+
 const userEmail = 'tbsolutions4@gmail.com';
 const mainEmail = 'tbsolutions3@gmail.com';
 const foreemail = 'tbsolutions55@gmail.com';
-*/
+
 const fs = require("fs");
 const PDFDocument = require("pdfkit");
 const path = require("path");
@@ -307,11 +307,11 @@ attachments.push({ filename: pdfFilename, path: pdfPath });
             to: email,
             bcc: [
                 { name: 'Traffic & Barrier Solutions, LLC', address: myEmail },
-              /*
+              
                 { name: 'Carson Speer', address: userEmail }, // Add the second Gmail address to BCC
                 { name: 'Bryson Davis', address: mainEmail },
                 { name: 'Jonkell Tolbert', address: foreemail }
-                */
+                
             ],
             subject: 'JOB APPLICATION REQUEST',
             html: `
