aaa17f75357042b92cc6adb901bc289ac0526aba
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2025-04-12 13:07:54 -0400

    Update emailConfig.js

--- utils/emailConfig.js
+++ utils/emailConfig.js
@@ -1,10 +1,12 @@
 const nodemailer = require('nodemailer');
+
 /*
 Go to the settings for your Google Account in the application or device you are trying to set up. 
 Replace your password with the 16-character password shown above.
 Just like your normal password, this app password grants complete access to your Google Account. 
 You won't need to remember it, so don't write it down or share it with anyone.
 */
+
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     secure: true,
@@ -16,8 +18,57 @@ const transporter = nodemailer.createTransport({
         pass: process.env.EMAIL_PASS
     },
     tls: {
-        rejectUnauthorized: false
+        rejectUnauthorized: true
     }
 });
 
-module.exports = transporter;
+const transporter2 = nodemailer.createTransport({
+    service: 'gmail',
+    secure: true,
+    logger: true,
+    debug: true,
+    secureConnection: false,
+    auth: {
+        user: process.env.EMAIL_USER_2,
+        pass: process.env.EMAIL_PASS_2
+    },
+    tls: {
+        rejectUnauthorized: true
+    }
+});
+
+const transporter3 = nodemailer.createTransport({
+    service: 'gmail',
+    secure: true,
+    logger: true,
+    debug: true,
+    secureConnection: false,
+    auth: {
+        user: process.env.EMAIL_USER_3,
+        pass: process.env.EMAIL_PASS_3
+    },
+    tls: {
+        rejectUnauthorized: true
+    }
+});
+
+const transporter4 = nodemailer.createTransport({
+    service: 'gmail',
+    secure: true,
+    logger: true,
+    debug: true,
+    secureConnection: false,
+    auth: {
+        user: process.env.EMAIL_USER_4,
+        pass: process.env.EMAIL_PASS_4
+    },
+    tls: {
+        rejectUnauthorized: true
+    }
+});
+
+module.exports = 
+    transporter,
+    transporter2,
+    transporter3,
+    transporter4;
