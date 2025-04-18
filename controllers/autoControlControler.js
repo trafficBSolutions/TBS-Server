058abd8ac065c6193faf4b597a875a1ff95ab48d
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2025-04-18 15:24:28 -0400

    Update autoControlControler.js

--- controllers/autoControlControler.js
+++ controllers/autoControlControler.js
@@ -1,13 +1,13 @@
 const ControlUser = require('../models/controluser');
 const transporter = require('../utils/emailConfig'); // Use transporter2 only
 const myEmail = 'tbsolutions9@gmail.com';
-/*
+
 const userEmail = 'tbsolutions4@gmail.com';
 const mainEmail = 'tbsolutions3@gmail.com';
 const foreemail = 'tbsolutions55@gmail.com';
 const foremanmail = 'tbsolutions77@gmail.com';
 const damienemail = 'tbsolutions14@gmail.com';
-*/
+
 const submitTrafficControlJob = async (req, res) => {
     try {
         const {
@@ -118,13 +118,13 @@ const submitTrafficControlJob = async (req, res) => {
             from: 'Traffic & Barrier Solutions LLC <tbsolutions9@gmail.com>',
             to: email,
             bcc: [{ name: 'Traffic & Barrier Solutions, LLC', address: myEmail },
-                  /*
+                  
                   { name: 'Carson Speer', address: userEmail }, // Add the second Gmail address to BCC
                   { name: 'Bryson Davis', address: mainEmail },
           { name: 'Jonkell Tolbert', address: foreemail },
           { name: 'Salvador Gonzalez', address: foremanmail},
           { name: 'Damien Diskey', address: damienemail}
-          */
+          
                  ],
             subject: 'TRAFFIC CONTROL JOB REQUEST',
             html: `
