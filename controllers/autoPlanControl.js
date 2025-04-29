743ea40827d67cf6c1f2caf11d0ade714ea86246
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2025-04-29 09:37:48 -0400

    Update autoPlanControl.js

--- controllers/autoPlanControl.js
+++ controllers/autoPlanControl.js
@@ -8,33 +8,22 @@ const foreemail = 'tbsolutions55@gmail.com';
 const submitPlan = async (req, res) => {
     try {
         const {
-            first,
-            last,
-            company,
+            name,
             email,
             phone,
+            company,
+            project,
             address,
             city,
             state,
             zip,
-            location,
             message
         } = req.body;
+        let structure;
 
-        const lat = req.body.lat;
-        const lng = req.body.lng;
-        const mapsUrl = `https://www.google.com/maps/search/?api=API_KEY&query=${lat},${lng}`;
-
-        let structureFile, structureImg;
-
-        if (req.files['structurefile']) {
-            structureFile = req.files['structurefile'][0].filename;
+        if (req.files['structure']) {
+            structure = req.files['structure'][0].filename;
         }
-
-        if (req.files['structureimg']) {
-            structureImg = req.files['structureimg'][0].filename;
-        }
-
         const isValidEmail = /\S+@\S+\.\S+/.test(email);
         if (!isValidEmail) {
             return res.status(400).json({
@@ -43,48 +32,46 @@ const submitPlan = async (req, res) => {
         }
 
         const newUser = await PlanUser.create({
-            first,
-            last,
+            name,
             company,
             email,
             phone,
+            company,
+            project,
             address,
             city,
             state,
             zip,
-            location,
-            structurefile: structureFile,
-            structureimg: structureImg,
+            structure: structure,
             message
         });
-
+        const attachments = [];
+        if (structure) {
+          attachments.push({ filename: structure, path: `./files/${structure}` });
+        }
         
         const mailOptions = {
             from: 'Traffic & Barrier Solutions LLC <tbsolutions9@gmail.com>',
             to: email,
-            bcc: [
-                { name: 'Traffic & Barrier Solutions, LLC', address: myEmail },
-                { name: 'Carson Speer', address: userEmail }, // Add the second Gmail address to BCC
-                { name: 'Bryson Davis', address: mainEmail },
-                { name: 'Jonkell Tolbert', address: foreemail }
-            ],
+            bcc: myEmail,
             subject: 'TRAFFIC CONTROL PLAN REQUEST',
-              html:`
+            html: `
             <html>
               <body style="margin: 0; padding: 20px; font-family: Arial, sans-serif; background-color: #e7e7e7; color: #000;">
                 <div style="max-width: 600px; margin: auto; background: #fff; padding: 20px; border-radius: 8px;">
                   <h1 style="text-align: center; background-color: #efad76; padding: 15px; border-radius: 6px;">TRAFFIC CONTROL PLAN REQUEST</h1>
                   
-                  <p>Hi <strong>${first}</strong>,</p>
+                  <p>Hi <strong>${name}</strong>,</p>
                   Your plan has been submitted! <br>
                   If you have any questions or concerns regarding your plan, please call (706) 263-0175.</p>
           
                   <h3>Summary:</h3>
                   <ul>
-                    <li><strong>Coordinator:</strong> ${first} ${last}</li>
+                    <li><strong>Coordinator:</strong> ${name}</li>
                     <li><strong>Email:</strong> ${email}</li>
                     <li><strong>Phone:</strong> ${phone}</li>
                     <li><strong>Company:</strong> ${company}</li>
+                    <li><strong>Job/Project Number:</strong> ${project}</li>
                     <li><strong>Job Site Address:</strong> ${address}, ${city}, ${state} ${zip}</li>
                   </ul>
                   <h3>Additional Info:</h3>
@@ -95,27 +82,18 @@ const submitPlan = async (req, res) => {
               </body>
             </html>
             `,
-            attachments: []
-        };
+            attachments
+          };
 
-        if (structureFile) {
+        if (structure) {
             mailOptions.attachments.push({
-                filename: structureFile,
-                path: `./files/${structureFile}`
+                filename: structure,
+                path: `./files/${structure}`
             });
         }
-
-        if (structureImg) {
-            mailOptions.attachments.push({
-                filename: structureImg,
-                path: `./files/${structureImg}`
-            });
-        }
-        
-
         transporter4.sendMail(mailOptions, (error, info) => {
             if (error) {
-                console.log('Error sending email notification:', error);
+                console.error('Error sending email notification:', error);
             } else {
                 console.log('Email notification sent:', info.response);
             }
