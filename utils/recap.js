19ce0869ee7f81972b500ec6f7994abb10fe1e5c
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-09-19 09:07:52 -0400

    Update recap.js

--- utils/recap.js
+++ utils/recap.js
@@ -1,20 +1,30 @@
 const axios = require('axios');
 
-const verifyRecaptcha = async (token) => {
+module.exports = async function verifyRecaptcha(req, res, next) {
   try {
-    const secretKey = process.env.RECAPTCHA_SECRET_KEY;
-    const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify`, null, {
-      params: {
-        secret: secretKey,
-        response: token,
-      }
-    });
+    const token =
+      req.body?.recaptchaToken ||
+      req.headers['x-recaptcha-token'] ||
+      req.query?.recaptchaToken;
 
-    return response.data.success;
+    if (!token) {
+      return res.status(400).json({ message: 'Missing reCAPTCHA token.' });
+    }
+
+    const secretKey = process.env.RECAPTCHA_SECRET_KEY; // make sure this env var exists!
+    const resp = await axios.post(
+      'https://www.google.com/recaptcha/api/siteverify',
+      null,
+      { params: { secret: secretKey, response: token } }
+    );
+
+    if (!resp.data?.success) {
+      return res.status(400).json({ message: 'reCAPTCHA verification failed.' });
+    }
+
+    return next();
   } catch (err) {
     console.error('reCAPTCHA verification error:', err);
-    return false;
+    return res.status(500).json({ message: 'reCAPTCHA verification error.' });
   }
 };
-
-module.exports = verifyRecaptcha;
