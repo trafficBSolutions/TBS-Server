f3b82a4686cfffa7d664cc405bbfdf512942d402
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-02-13 10:54:42 -0500

    Update autoQuoteRoute.js

--- routes/autoQuoteRoute.js
+++ routes/autoQuoteRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { submitQuote, getMonthlyQuotes, getDailyQuotes } = require('../controllers/autoQuoteControl');
+const { submitQuote, getMonthlyQuotes, getDailyQuotes, resendQuote } = require('../controllers/autoQuoteControl');
 
 router.post('/api/quote', submitQuote);
 router.get('/api/quotes/month', getMonthlyQuotes);
 router.get('/api/quotes/day', getDailyQuotes);
+router.post('/api/quotes/:id/resend', resendQuote);
 
 module.exports = router;
