5c35ed88fa1327e5904a4a253f2367c7e1a4eeb1
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-04-08 13:15:49 -0400

    Update disciplineRoute.js

--- routes/disciplineRoute.js
+++ routes/disciplineRoute.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { submitDiscipline, listByMonth, listByDate, getDisciplinePDF } = require('../controllers/disciplineController');
+const {
+  addEmployee, listEmployees, deleteEmployee, getEmployeePoints,
+  submitDiscipline, listByMonth, listByDate, getDisciplinePDF
+} = require('../controllers/disciplineController');
 
+// Employee roster
+router.get('/employees', listEmployees);
+router.post('/employees', addEmployee);
+router.delete('/employees/:id', deleteEmployee);
+router.get('/employees/:id/points', getEmployeePoints);
+
+// Discipline actions
 router.post('/', submitDiscipline);
 router.get('/month', listByMonth);
 router.get('/', listByDate);
