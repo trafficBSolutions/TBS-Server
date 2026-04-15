29ad9eb7da1c7efccd66aa14c82f7ef482148b6d
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-04-15 14:24:48 -0400

    Create signShopJob.js

---
+++ models/signShopJob.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const signShopJobSchema = new mongoose.Schema({
+  title: { type: String, required: true, trim: true },
+  customer: { type: String, trim: true },
+  description: { type: String, trim: true },
+  date: { type: String, required: true },
+  completed: { type: Boolean, default: false },
+  author: { type: String, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('SignShopJob', signShopJobSchema);
