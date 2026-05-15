588b693722d1c0fdc9e105b6c747e0211b1c0521
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-05-15 10:31:40 -0400

    Create TimeClockEmployee model with schema

---
+++ models/timeClockEmployee.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const TimeClockEmployeeSchema = new mongoose.Schema({
+  firstName: { type: String, trim: true, required: true },
+  lastName:  { type: String, trim: true, required: true },
+  pin:       { type: String, required: true, unique: true },
+  active:    { type: Boolean, default: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('TimeClockEmployee', TimeClockEmployeeSchema);
