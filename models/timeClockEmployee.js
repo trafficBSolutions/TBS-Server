ad15f76bd54f2f1470c85760c6c1426884afe366
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-05-15 10:51:29 -0400

    Add position field to TimeClockEmployee schema

--- models/timeClockEmployee.js
+++ models/timeClockEmployee.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const TimeClockEmployeeSchema = new mongoose.Schema({
   firstName: { type: String, trim: true, required: true },
   lastName:  { type: String, trim: true, required: true },
+  position:  { type: String, trim: true, enum: ['Flagger', 'Driver', 'Foreman'], required: true },
   pin:       { type: String, required: true, unique: true },
   active:    { type: Boolean, default: true }
 }, { timestamps: true });
