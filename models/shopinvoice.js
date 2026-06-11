e737bc55111f16cef811e738951e7369b83d4a22
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-06-11 15:57:58 -0400

    Create shopInvoice model with schema definition

---
+++ models/shopinvoice.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const shopInvoiceSchema = new mongoose.Schema({
+  invoiceNumber: { type: String, required: true },
+  date: { type: String, required: true },
+  company: { type: String, required: true },
+  customer: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: String,
+  isTaxExempt: Boolean,
+  taxExemptNumber: String,
+  rows: [{
+    item: String,
+    description: String,
+    taxable: Boolean,
+    qty: Number,
+    unitPrice: Number
+  }],
+  computed: {
+    subtotal: Number,
+    taxDue: Number,
+    ccFee: Number,
+    total: Number
+  },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('ShopInvoice', shopInvoiceSchema);
