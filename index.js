09f612fef282302ce612133894287f518308528f
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-06-15 15:21:31 -0400

    Add printCostRoute to main routes

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ app.use('/', require('./routes/autoRentalRoute'));
 app.use('/', require('./routes/autoContactRoute'));
 app.use('/', require('./routes/autoQuoteRoute'));
 app.use('/', require('./routes/shopInvoiceRoute'));
+app.use('/', require('./routes/printCostRoute'));
 app.use('/', require('./routes/adminRoute'));
 app.use(require('./routes/invoiceRoute'));
 app.use(require('./routes/payCard'));  // if using Stripe
