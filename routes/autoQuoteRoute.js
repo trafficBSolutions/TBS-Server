2fdae0139e8397c7b36cb280680b4f019505edb2
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-06-29 10:40:48 -0400

    Add approveQuote route to autoQuoteRoute

--- routes/autoQuoteRoute.js
+++ routes/autoQuoteRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { submitQuote, getMonthlyQuotes, getDailyQuotes, resendQuote, submitInvoice } = require('../controllers/autoQuoteControl');
+const { submitQuote, getMonthlyQuotes, getDailyQuotes, resendQuote, submitInvoice, approveQuote } = require('../controllers/autoQuoteControl');
 
 router.post('/api/quote', submitQuote);
 router.post('/api/invoice', submitInvoice);
 router.get('/api/quotes/month', getMonthlyQuotes);
 router.get('/api/quotes/day', getDailyQuotes);
 router.post('/api/quotes/:id/resend', resendQuote);
+router.get('/api/quotes/:id/approve', approveQuote);
 
 module.exports = router;
