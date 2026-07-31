2183135f2ec337cd595bdc0005490bc16ede3692
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-07-31 15:11:02 -0400

    Refactor submitApply function and email logic

--- controllers/autoApplyControl.js
+++ controllers/autoApplyControl.js
@@ -170,16 +170,16 @@ attachments.push({ filename: pdfFilename, path: pdfPath });
         const mailOptions = {
             from: 'Traffic & Barrier Solutions LLC <tbsolutions9@gmail.com>',
             to: email,
-            cc: dasia,
+            /* cc: dasia,*/
             bcc: [
                 { name: 'Traffic & Barrier Solutions, LLC', address: myEmail },
-                
+                /*
                 { name: 'Carson Speer', address: userEmail }, // Add the second Gmail address to BCC
                 { name: 'Bryson Davis', address: mainEmail },
                 { name: 'Jonkell Tolbert', address: foreemail },
                 { name: 'Salvador Gonzalez', address: foremanmail },
                 { name: 'Damien Diskey', address: damienemail},
-                 
+                 */
             ],
             subject: 'JOB APPLICATION REQUEST',
          html: `
